1a613d69f37570d0f3b521b569d8557a4bb9a780
혜준 <167407164+HyejunKoo@users.noreply.github.com>
2025-08-09 13:07:03 +0900

    [Bronze IV] Title: 영수증, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int X,N,a,b;
+    int sum=0;
+    
+    cin >> X;
+    cin >> N;
+    
+    for(int i=0; i<N; i++){
+        cin >> a >> b;
+        sum+= a*b;
+    }
+    if (sum == X){printf("Yes");}
+        
+    else{cout << "No";} 
+        
+    
+        return 0;
+    
+}
